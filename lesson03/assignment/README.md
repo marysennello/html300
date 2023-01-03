0d409971aec957ecbb42e27d7608b4bf246d6c80
Mary Sennello <marysenn@uw.edu>
2023-01-02 17:01:28 -0800

    created cards

--- lesson03/assignment/README.md
+++ lesson03/assignment/README.md
@@ -11,6 +11,7 @@
 -   Leave gulp running. As you work with the files, browsersync will watch your scss and HTML files and reload the page in the browser on changes
 - 	Use the contents of `data/data.json` for your data -- remember: you can copy the array into your JS file and set it to a variable in your Javascript file to work with
 - 	Migrate your previous SCSS/HTML from assignment 02 for your component (you can copy the corresponding files from your previous assignment to this one so you'll have the HTML/SCSS of the card set up)
+
 - 	In your JS, convert the component to a template with ES6 string templating and loop through the JSON data, creating components for each object in the data
 - 	Output the templated data onto the `.template-hook` section of the index.html page
 - 	*BONUS*: Add to your component another section for the `codeLanguages` key in the data. Loop through the `codeLanguages` array and add the contents to your new section in the component
---
+++ lesson03/assignment/profilecards.js
@@ -0,0 +1,64 @@
+// Code that requires the DOM be loaded should not be run until the DOM is loaded
+document.addEventListener("DOMContentLoaded", function() {
+
+      //JSON informationconst
+      profilesJSON = '[  {    "name": "Paolo Maldini",    "jobTitle": "Front End Developer",    "company": "Web Weavers",    "experience": "3 years",    "school": "University of Washington",    "major": "Marketing",    "email": "paolo@example.com",    "linkedInUrl": "paolo.linkedinprofile.com",    "codeLanguages": [      " HTML", " CSS", " JavaScript", " Node", " Express"    ]  },  {    "name": "Barbara Bonansea",    "jobTitle": "Software Engineer",    "company": "Excellence in the Cloud",    "experience": "12 years",    "school": "University of Southern California",    "major": "Computer Science",    "email": "barbara@example.com",    "linkedInUrl": "barbara.linkedinprofile.com",    "codeLanguages": [" HTML", " JavaScript", " C", " Go"    ]  },  {    "name": "Javier Hernandez",    "jobTitle": "User Experience Engineer",    "company": "Web Sites and More",    "experience": "5 years",    "school": "Seattle University",    "major": "Performing Arts",    "email": "javier@example.com",    "linkedInUrl": "javier.linkedinprofile.com",    "codeLanguages": [ " HTML", " CSS" ]  },  {    "name": "Maribel Dominguez",    "jobTitle": "Front End Engineer",    "company": "Bits and Bytes",    "experience": "6 years",    "school": "University of Washington",    "major": "Mechanical Engineering",    "email": "maribel@example.com",    "linkedInUrl": "maribel.linkedinprofile.com",    "codeLanguages": [ "HTML", " CSS", " JavaScript",      " React", " Vue", " Redux"]  }]'
+      const profiles = JSON.parse(profilesJSON);
+
+      console.log(Object.entries(profiles));
+
+      //where to put the cards:
+      const cardSection = document.querySelector('section')
+      //testing testing
+      cardSection.innerHTML = ` <h2>this is working</h2> `
+      console.log(cardSection);
+
+
+      //NEED: div.card-container, div.left, div.right, img.profile-img, h2, p.caption, span.bold, p
+      profiles.forEach(function(el) {
+        //card container for each
+        let cardContainer = document.createElement('div');
+        cardContainer.classList.add('card-container');
+
+      //left side of card
+        let leftSide = document.createElement('div');
+        leftSide.classList.add('left');
+        //prof img
+        let profileImg = document.createElement('img');
+        profileImg.classList.add('profile-img');
+        profileImg.src = ('img/headshot.jpg')
+        //name
+        let name = document.createElement('h2');
+        name.innerHTML = `${el.name}`
+        //caption/ title
+        let title = document.createElement('p');
+        title.classList.add('caption');
+        title.innerHTML = `${el.jobTitle}`
+
+      //right side of card
+      let rightSide = document.createElement('div');
+      rightSide.classList.add('right');
+      //profile info
+      let info = document.createElement('ul')
+      info.innerHTML = `
+      <li><span class="bold">Company: </span> ${el.company}</li>
+      <li><span class="bold">Experience: </span> ${el.experience}</li>
+      <li><span class="bold">School: </span> ${el.school}</li>
+      <li><span class="bold">Major: </span> ${el.major}</li>
+      <li><span class="bold">Email: </span> ${el.email}</li>
+      <li><span class="bold">Code Languages: </span>  ${el.codeLanguages}</li>
+      `
+      //linkedIn bug and links
+      let networking = document.createElement('p')
+      networking.innerHTML = `
+      <img class="bug" src="img/linkedin.svg"> ${el.linkedInUrl}
+      `
+        leftSide.append(profileImg, name, title);
+        rightSide.append(info, networking);
+        cardContainer.append(leftSide, rightSide);
+        cardSection.append(cardContainer);
+      })
+
+
+      //final closing bracket
+    })
