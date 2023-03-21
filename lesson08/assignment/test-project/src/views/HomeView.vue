da17859a5f7541d42d52c87f1523633b2149efea
Mary Sennello <marysenn@uw.edu>
2023-03-21 16:08:36 -0700

    added views files

---
+++ lesson08/assignment/test-project/src/views/HomeView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="home">
+  <!--  <img alt="Vue logo" src="../assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <main class="main">
+    <section>
+      <h2>Landing page with some content</h2>
+      <h3>check out some cool tool tips in the footer</h3>
+      <p class="lead p-intro"> content WIP </p>
+      <h4>
+        <button class="korokbutton" v-on:click="show = !show">Examine?</button>
+      </h4>
+      <img v-if="show" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/the-legend-of-zelda-hd/c/c3/KP_FairyDust.jpg?width=2240" class="korokpuzzle">
+
+
+      <img v-else src="https://cdn.vox-cdn.com/thumbor/vFQLJ3ZDw2DYKmL54jvv1H0O_Pg=/0x0:1920x1080/1520x1013/filters:focal(807x387:1113x693):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/55337577/cover_1_0000_Layer_6.0.jpg" class="korokpuzzle">
+
+    </section>
+  </main>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  //components: {
+  //  HelloWorld
+  //},
+  data (){
+    return {
+    show: true,
+    }
+  }
+}
+
+
+</script>
+
+
+<style>
+.korokpuzzle {
+max-width: 80%;
+border-radius: 50px;
+}
+.korokbutton {
+color: black;
+}
+
+</style>
---
+++ lesson08/assignment/test-project/src/views/img-page.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="img-page">
+    <main class="main">
+      <section>
+        <h2>Hyrule's Champions and the Elements</h2>
+        <h3> based purely on what my heart tells me but this content is just so obvious to me</h3>
+        <p class="lead p-intro"> content WIP </p>
+      </section>
+
+      <section v-if="champions">
+        <ChampionPage
+          v-for="(champion) in champions"
+          :key="champion.name"
+          :url="champion.url"
+          :name="champion.name"
+          :element="champion.element"
+          :sign="champion.signs"
+          :description="champion.description"
+          >
+          </ChampionPage>
+      </section>
+<!--
+
+      <div class="champion-container" v-for="(champion, index) in champions" :key="index">
+
+
+        <div class="champion-img">
+          <img v-bind:src="champion.url" :alt="champion.name" class="img-fluid">
+        </div>
+        <div class="champion-description">
+          <h2>{{champion.element}} Sign Rep: {{champion.name}}</h2>
+          <h3>{{champion.signs}}</h3>
+          <p class="text-left">{{champion.description}}</p>
+        </div>
+
+      </div>-->
+    </main>
+  </div>
+</template>
+
+<script>
+  import Urbosa from "../assets/imgs-champions/urbosa-botw-intro.png"
+  import Daruk from "../assets/imgs-champions/daruk-botw-intro.png"
+  import Revali from "../assets/imgs-champions/revali-botw-intro.png"
+  import Mipha from "../assets/imgs-champions/mipha-botw-intro.png"
+  import ChampionPage from "../components/Champion.vue"
+
+export default {
+  name: 'App',
+  components: {
+    ChampionPage
+  },
+  data (){
+    return {
+    //img-Champions
+      champions: [
+        {
+          name: 'Urbosa',
+          url : Urbosa,
+          element: 'Fire',
+          signs: 'Aries, Leo, Sagitarius',
+          description: 'Chief of the Gerudo, a natural leader, capable of manipulating lightening, and trouble containing her emotions, there is no doubt that Urbosa represents the Fire signs. duty, organization, urbosa\'s fury, fiercely loyal.',
+        },
+        {
+          name: 'Daruk',
+          url : Daruk,
+          element: 'Earth',
+          signs: 'Taurus, Virgo, Capricorn',
+          description: 'Daruk, and his race of Goron, are undeniably of the Earth. Their strength and resiliance strongly reverberate with Earth signs. Sometimes stubborn, looking for the bigger picture, and moving purposefully so as not to misstep or make a wrong decision are things you could say about both.',
+        },
+        {
+          name: 'Revali',
+          url : Revali,
+          element: 'Air',
+          signs: 'Gemini, Libra, Aquarius',
+          description: 'Air signs are often seen as intellectual and analytical, and Revali is exactly that. The weapons of the Rito are known for precision and distance, and Revali\'s personality matches the sometimes boastful and abrasive personalities of Air signs. Hide insecurities. "Coasting through life" "winging it" updraft to soar high in the air. his appearance is based on a bird of prey',
+        },
+        {
+          name: 'Mipha',
+          url : Mipha,
+          element: 'Water',
+          signs: 'Pisces, Cancer, Scorpio',
+          description: 'Let\'s start with the obvious, ok? Mipha is the princess of the Zora, an aquatic race adept at swimming and thriving underwater.',
+        },
+      ], //end champions
+
+
+    }//end return
+  }//end data
+}
+</script>
+<style>
+</style>
